830bcb84749d43b5e13dca129bd1a350672e3cfc
EFIF\aabd <aabd@aabc.dk>
2022-09-09 13:07:56 +0200

    Committing all changes in multirepo

--- Class1.cs
+++ Class1.cs
@@ -5,5 +5,10 @@ namespace SubProject1
     public class Class1
     {
         private string something;
+        public Class1()
+        {
+            something = "Something SUb1 ";
+            Console.WriteLine(something);
+        }
     }
 }
